fef836d9f39cc2cade2080e29e732e13caad4904
Sandwitchy <andrea.fratani@gmail.com>
2019-12-10 12:23:32 +0100

    Add connexion system

--- src/main/java/reservation/Controller/HomeController.java
+++ src/main/java/reservation/Controller/HomeController.java
@@ -5,7 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-	
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
 	@GetMapping({"/", "/accueil"})
 	public String accueil() {
 		return "accueil";
