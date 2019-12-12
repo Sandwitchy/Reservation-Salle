52c46621f760013cce3cc5093a36032e2d58f839
Sandwitchy <andrea.fratani@gmail.com>
2019-12-12 14:00:35 +0100

    Security update

--- src/main/java/reservation/Controller/HomeController.java
+++ src/main/java/reservation/Controller/HomeController.java
@@ -2,15 +2,49 @@ package reservation.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
-	@GetMapping("/login")
-	public String login() {
-		return "login";
+	@GetMapping({"/","/welcome"})
+	public ModelAndView welcomePage() {
+		ModelAndView model = new ModelAndView();
+		model.addObject("title", "Spring Security Custom Login Form");
+		model.addObject("message", "This is welcome page!");
+		model.setViewName("accueil");
+		return model;
 	}
-	@GetMapping({"/", "/accueil"})
-	public String accueil() {
-		return "accueil";
+
+	@GetMapping("/admin")
+	public ModelAndView adminPage() {
+
+		ModelAndView model = new ModelAndView();
+		model.addObject("title", "Spring Security Custom Login Form");
+		model.addObject("message", "This is protected page!");
+		model.setViewName("admin");
+
+		return model;
+
+	}
+
+	//Spring Security see this :
+	@GetMapping("/login")
+	public ModelAndView login(
+		@RequestParam(value = "error", required = false) String error,
+		@RequestParam(value = "logout", required = false) String logout) {
+
+		ModelAndView model = new ModelAndView();
+		if (error != null) {
+			model.addObject("error", "Invalid username and password!");
+		}
+
+		if (logout != null) {
+			model.addObject("msg", "You've been logged out successfully.");
+		}
+		model.setViewName("login");
+
+		return model;
+
 	}
 }
