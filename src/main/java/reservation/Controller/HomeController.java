3a39a64f06679e8050c9bbf21cb4fc5284703f1b
Xinjey <45387417+Xinjey@users.noreply.github.com>
2019-12-13 11:36:00 +0100

    init

--- src/main/java/reservation/Controller/HomeController.java
+++ src/main/java/reservation/Controller/HomeController.java
@@ -1,53 +1,13 @@
 package reservation.Controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class HomeController {
-	@GetMapping({"/","/welcome"})
-	public ModelAndView welcomePage() {
-		ModelAndView model = new ModelAndView();
-		model.addObject("title", "Spring Security Custom Login Form");
-		model.addObject("message", "This is welcome page!");
-		model.setViewName("accueil");
-		return model;
+	
+	@GetMapping({"/", "/accueil"})
+	public String accueil() {
+		return "accueil";
 	}
-	@GetMapping("/admin")
-	public ModelAndView adminPage() {
-
-		ModelAndView model = new ModelAndView();
-		model.addObject("title", "Spring Security Custom Login Form");
-		model.addObject("message", "This is protected page!");
-		model.setViewName("admin");
-
-		return model;
-
-	}
-
-	@GetMapping("/accessdenied")
-	public RedirectView denied(ModelMap model) {
-		model.addAttribute("error", "Les informations entrées sont soit fausse soit incorrect. Veuillez recommencez.");
-		return new RedirectView("login");
-	}
-	@GetMapping("/logout")
-    public RedirectView logout(ModelMap model) {
-		model.addAttribute("logout", "Vous avez bien été déconnecté");
-		return new RedirectView("login");
-    }
-	//Spring Security see this :
-	@GetMapping("/login")
-    public String login(Model model, String error, String logout) {
-        if (error != null)
-            model.addAttribute("error", error);
-
-        if (logout != null)
-            model.addAttribute("msg", logout);
-
-        return "login";
-    }
 }
---
+++ src/main/java/reservation/Model/Salle.java
@@ -0,0 +1,23 @@
+/*
+package reservation.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Salle {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String numeroSalle;
+	
+	public Salle() {
+		
+	}
+	public Salle(String numeroSalle) {
+		this.numeroSalle = numeroSalle;
+	}
+}
+*/
